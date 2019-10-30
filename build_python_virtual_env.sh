e59b52fadd199927e2e26aa120222cd062e13660
pingsutw <pingsutw@gmail.com>
2019-10-30 17:04:53 +0800

    Add virtualenv script

---
+++ build_python_virtual_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://files.pythonhosted.org/packages/33/bc/fa0b5347139cd9564f0d44ebd2b147ac97c36b2403943dbee8a25fd74012/virtualenv-16.0.0.tar.gz
+tar xf virtualenv-16.0.0.tar.gz
+
+# Make sure to install using Python 3, as TensorFlow only provides Python 3 artifacts
+python3 virtualenv-16.0.0/virtualenv.py opencvdl
+. opencvdl/bin/activate
+pip3 install tensorflow-gpu
+pip3 install pyqt5
+pip3 install matplotlib
+pip3 install opencv-contrib-python
+deactivate
