9e6d19ab3ecfb1217fd46f5c8243b1957d36dfdf
pingsutw <pingsutw@gmail.com>
2019-11-01 18:58:43 +0800

    show training and validating result

--- homework/hw1/mnist.py
+++ homework/hw1/mnist.py
@@ -10,7 +10,6 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel,\
  QPushButton, QVBoxLayout, QHBoxLayout, QDialog, QGroupBox, QGridLayout, QRadioButton, QLineEdit
 from PyQt5 import QtWidgets, QtCore
 import easygui
-import numpy as np
 import cv2
 
 # Tensorflow 
@@ -49,7 +48,7 @@ class Window(QWidget):
 
         bt3 = QPushButton("5.4 Show Training Result")
         bt3.setFixedSize(300,70)
-        #bt3.clicked.connect(blending)
+        bt3.clicked.connect(trainingResult)
         l0 = QLabel()
         l0.setText("Test Image Index:")
         line0 = QLineEdit()
@@ -99,18 +98,94 @@ learning_rate = 0.001
 BATCH_SIZE = 32
 EPOCHS = 10
 
+
+def getModel():
+    inputs = keras.Input(shape=(784,), name='img')
+    x = layers.Dense(64, activation='relu')(inputs)
+    x = layers.Dense(64, activation='relu')(x)
+    outputs = layers.Dense(10, activation='softmax')(x)
+    
+    model = keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
+    return model
+    #print(model.summary())
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
 def trainOneEpoch(checked):
-	inputs = keras.Input(shape=(784,), name='img')
-	x = layers.Dense(64, activation='relu')(inputs)
-	x = layers.Dense(64, activation='relu')(x)
-	outputs = layers.Dense(10, activation='softmax')(x)
-	
-	model = keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
-	print(model.summary())
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255
+
+    model = getModel()
+    model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=keras.optimizers.RMSprop(),
+              metrics=['accuracy'])
+
+    lossHistory = LossHistory()
+    history = model.fit(x_train, y_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=1,
+                    validation_split=0.2,
+                    callbacks=[lossHistory])
+    # print(lossHistory.losses)
+    print(history.history.keys())
+    plt.plot(lossHistory.losses)
+    plt.title('epoch [0/50]')
+    plt.ylabel('loss')
+    plt.xlabel('iteration')
+    plt.legend(['train'], loc='upper left')
+    plt.show()
+   
+    test_scores = model.evaluate(x_test, y_test, verbose=2)
+    print('Test loss:', test_scores[0])
+    print('Test accuracy:', test_scores[1])
+    model.save('model/mnist-epoch1.h5')
+
+def trainingResult():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255
+
+    model = getModel()
+    model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=keras.optimizers.RMSprop(),
+              metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=50,
+                    validation_split=0.2)
+
+    accuracy=plt.figure(1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Accuracy')
+    plt.ylabel('%')
+    plt.xlabel('epoch')
+    plt.legend(['Training', 'Testing'], loc='lower right')
+    accuracy.show()
+    
+    loss=plt.figure(2)
+    plt.plot(history.history['loss'])
+    plt.title('Training Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['Training', 'Testing'], loc='lower right')
+    loss.show()
+
+    test_scores = model.evaluate(x_test, y_test, verbose=2)
+    print('Test loss:', test_scores[0])
+    print('Test accuracy:', test_scores[1])
+    model.save('model/mnist.h5')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    clock = Window()
-    clock.show()
+    window = Window()
+    window.show()
     sys.exit(app.exec_())
     sys.exit()
